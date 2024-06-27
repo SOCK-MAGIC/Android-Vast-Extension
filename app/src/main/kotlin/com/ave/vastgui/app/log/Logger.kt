9534c68ddfd83ce35e6950e98dee57b43b960cee
Vast Gui <sakurajimamai2020@qq.com>
2024-06-28 01:03:06 +0800

    update(log):对日志框架进行调整

--- app/src/main/kotlin/com/ave/vastgui/app/log/Logger.kt
+++ app/src/main/kotlin/com/ave/vastgui/app/log/Logger.kt
@@ -16,8 +16,11 @@
 
 package com.ave.vastgui.app.log
 
+import com.ave.vastgui.tools.log.android
 import com.ave.vastgui.tools.manager.filemgr.FileMgr
 import com.log.vastgui.core.LogFactory
+import com.log.vastgui.core.base.LogLevel
+import com.log.vastgui.core.base.LogStore
 import com.log.vastgui.core.base.Logger
 import com.log.vastgui.core.format.OnlyMsgFormat
 import com.log.vastgui.core.getLogFactory
@@ -27,7 +30,9 @@ import com.log.vastgui.core.json.JacksonConverter
 import com.log.vastgui.core.plugin.LogJson
 import com.log.vastgui.core.plugin.LogPretty
 import com.log.vastgui.core.plugin.LogPrinter
+import com.log.vastgui.core.plugin.LogStorage
 import com.log.vastgui.core.plugin.LogSwitch
+import com.log.vastgui.mars.base.MarsWriteMode
 import com.log.vastgui.mars.mars
 import java.io.File
 
@@ -49,7 +54,7 @@ val mLogFactory: LogFactory = getLogFactory {
         open = true
     }
     install(LogPrinter) {
-        logger = Logger.mars(logDir, logCache, OnlyMsgFormat)
+        logger = Logger.mars(logDir, logCache, mode = MarsWriteMode.Async, pubKey = "235080c1712a9072c68483609b46ef802d9ef8737763cfbade76f895732b32ce225f80e3df8e3c511d033df7fa70fc9699525df0ffc3df5a7072730480d383a9")
     }
     install(LogJson) {
         converter = gson
@@ -59,21 +64,21 @@ val mLogFactory: LogFactory = getLogFactory {
     }
 }
 
-//@JvmField
-//val mLogFactory: LogFactory = getLogFactory {
-//    install(LogSwitch) {
-//        open = false
-//    }
-//    install(LogPrinter) {
-//        logger = Logger.android()
-//    }
-//    install(LogJson) {
-//        converter = gsonConverter
-//    }
-//    install(LogPretty) {
-//        converter = gsonConverter
-//    }
-//    install(LogStorage) {
-//        logStore = LogStore.android()
-//    }
-//}
+@JvmField
+val mLogFactory2: LogFactory = getLogFactory {
+    install(LogSwitch) {
+        open = false
+    }
+    install(LogPrinter) {
+        logger = Logger.android()
+    }
+    install(LogJson) {
+        converter = gson
+    }
+    install(LogPretty) {
+        converter = gson
+    }
+    install(LogStorage) {
+        logStore = LogStore.android()
+    }
+}
